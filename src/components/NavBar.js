29eb4aae30d58f3765069ba5a366df3df3edf50d
wayne-AF <wayne.anthony.feeney@gmail.com>
2023-04-08 19:32:22 +0000

    add styling to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,36 @@
 import React from 'react'
 import { Navbar, Container, Nav } from 'react-bootstrap'
 import logo from '../assets/hissr-logo-2.png'
+import styles from '../styles/NavBar.module.css'
 
 const NavBar = () => {
   return (
-    <Navbar bg="light" expand="md" fixed="top">
-      <Container>
-        <Navbar.Brand><img src={logo} alt="logo" height="50"/></Navbar.Brand>
+    
+      <Navbar className={styles.NavBar} expand="md" fixed="top">
+        <Container>
+        <Navbar.Brand>
+          <img src={logo} alt="logo" height="50" />
+        </Navbar.Brand>
+
+        {/* <Navbar.Brand className={styles.navbarName}>hissr</Navbar.Brand> */}
+
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link>Home</Nav.Link>
-            <Nav.Link>Sign in</Nav.Link>
-            <Nav.Link>Sign up</Nav.Link>
+          <Nav className="ml-auto text-right">
+            <Nav.Link className={styles.linkText}>
+              <i class="fa-solid fa-house"></i>Home
+            </Nav.Link>
+            <Nav.Link className={styles.linkText}>
+              <i class="fa-solid fa-right-to-bracket"></i>Sign in
+            </Nav.Link>
+            <Nav.Link className={styles.linkText}>
+              <i class="fa-solid fa-user-plus"></i>Sign up
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
-      </Container>
-    </Navbar>
+        </Container>
+      </Navbar>
+    
   );
 };
 
