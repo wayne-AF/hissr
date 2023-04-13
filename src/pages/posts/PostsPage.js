7ed366a71707fe83f40d525feed3d3ebc266ff90
wayne-AF <wayne.anthony.feeney@gmail.com>
2023-04-13 13:12:39 +0000

    create fetchMoreData function to request more data from API to allow continuous scrolling through lists of content, implement on PostsPage.js

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -12,6 +12,7 @@ import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
 import NoResults from "../../assets/no-results.png"
 import Asset from "../../components/Asset";
+import { fetchMoreData } from "../../utils/utils";
 
 function PostsPage({ message, filter = ""}) {
     const [posts, setPosts] = useState({ results: [] })
@@ -59,11 +60,20 @@ function PostsPage({ message, filter = ""}) {
         </Form>
         {hasLoaded ? (
             <>
-                {posts.results.length 
-                ? posts.results.map((post) => (
-                        <Post key={post.id} {...post} setPosts={setPosts} />
-                    ))
-                : (<Container className={appStyles.Content}>
+                {posts.results.length ? (
+                    <InfiniteScroll 
+                        children={
+                            posts.results.map((post) => (
+                                <Post key={post.id} {...post} setPosts={setPosts} />
+                            ))
+                        }
+                        dataLength={posts.results.length}
+                        loader={<Asset spinner />}
+                        hasMore={!!posts.next}
+                        next={() => fetchMoreData(posts, setPosts)}
+                    />
+                    
+                ) : (<Container className={appStyles.Content}>
                     <Asset src={NoResults} message={message}/>    
                 </Container>)}
             </>
