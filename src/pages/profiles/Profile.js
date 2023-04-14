947926314466f336a78f166703a8c6777e78d3cd
wayne-AF <wayne.anthony.feeney@gmail.com>
2023-04-14 17:19:57 +0000

    add follow/unfollow buttons for profile component desktop view

--- src/pages/profiles/Profile.js
+++ src/pages/profiles/Profile.js
@@ -1,13 +1,14 @@
 import React from "react";
-import styles from "../../styles.Profile.module.css";
+import styles from "../../styles/Profile.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import Avatar from "../../components/Avatar";
+import { Button } from "react-bootstrap";
 
 const Profile = (props) => {
-    const { profile, mobile, imageSize = 55 } = props;
-    const { id, follow_id, image, owner } = profile
+    const { profile, mobile, imageSize = 40 } = props;
+    const { id, following_id, image, owner } = profile
 
     const currentUser = useCurrentUser()
     const is_owner = currentUser?.username === owner
@@ -23,6 +24,25 @@ const Profile = (props) => {
             </div>
             <div className={`mx-2 ${styles.WordBreak}`}>
                 <strong>{owner}</strong>
+            </div>
+            <div className={`text-right ${!mobile && 'ml-auto'}`}>
+                {!mobile && currentUser && !is_owner && (
+                    following_id ? (
+                        <Button 
+                            className={btnStyles.Button}
+                            onClick={()=>{}}
+                        >
+                            unfollow
+                        </Button>
+                    ) : (
+                        <Button
+                            className={btnStyles.Button}
+                            onClick={()=>{}}
+                        >
+                            follow
+                        </Button>
+                    )
+                )}
             </div>
         </div>
     )
