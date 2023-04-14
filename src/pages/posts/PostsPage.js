dfa9ca6e70ee881bdfa51db10f34d604819e4719
wayne-AF <wayne.anthony.feeney@gmail.com>
2023-04-14 16:38:19 +0000

    create PopularProfiles to display list of most popular profiles

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -14,6 +14,7 @@ import NoResults from "../../assets/no-results.png"
 import Asset from "../../components/Asset";
 import { fetchMoreData } from "../../utils/utils";
 import InfiniteScroll from "react-infinite-scroll-component";
+import PopularProfiles from "../profiles/PopularProfiles";
 
 function PostsPage({ message, filter = ""}) {
     const [posts, setPosts] = useState({ results: [] })
@@ -85,7 +86,7 @@ function PostsPage({ message, filter = ""}) {
         )}
       </Col>
       <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
-        <p>Popular profiles for desktop</p>
+        <PopularProfiles />
       </Col>
     </Row>
   );
---
+++ src/pages/profiles/PopularProfiles.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react'
+import appStyles from '../../App.module.css'
+import { Container } from 'react-bootstrap'
+import { axiosReq } from '../../api/axiosDefaults'
+import { useCurrentUser } from '../../contexts/CurrentUserContext'
+
+const PopularProfiles = () => {
+    const [profileData, setProfileData] = useState({
+        popularProfiles : { results: [] }
+    })
+
+    const {popularProfiles} = profileData
+    const currentUser = useCurrentUser()
+
+    useEffect(() => {
+        const handleMount = async () => {
+            try {
+                const {data} = await axiosReq.get(
+                    '/profiles/?ordering=-followers_count'
+                )
+                setProfileData(prevState => ({
+                    ...prevState,
+                    popularProfiles: data
+                }))
+            } catch(err){
+                console.log(err)
+            }
+        }
+        handleMount()
+    }, [currentUser])
+
+  return (
+    <Container className={appStyles.Content}>
+        <p>Most followed profiles</p>
+    </Container>
+  )
+}
+
+export default PopularProfiles
